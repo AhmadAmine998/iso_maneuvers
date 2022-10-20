e0aee1a12fd7e788ceee64f04276a94391b66488
ahmadamine998 <ahmadamine998@gmail.com>
2022-10-20 17:07:02 -0400

    Added utils for bezier

---
+++ utils/bezier_interpolation.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy
+
+def __bernstein_poly(i, n, t):
+    """
+     The Bernstein polynomial of n, i as a function of t
+    """
+
+    return scipy.special.comb(n, i) * ( t**(n-i) ) * (1 - t)**i
+
+def interp_bezier(points, nTimes=1000):
+    """
+       Given a set of control points, return the
+       bezier curve defined by the control points.
+
+       points should be a list of lists, or list of tuples
+       such as [ [1,1], 
+                 [2,3], 
+                 [4,5], ..[Xn, Yn] ]
+        nTimes is the number of time steps, defaults to 1000
+
+        See http://processingjs.nihongoresources.com/bezierinfo/
+    """
+
+    nPoints = len(points)
+    xPoints = np.array([p[0] for p in points])
+    yPoints = np.array([p[1] for p in points])
+
+    t = np.linspace(0.0, 1.0, nTimes)
+
+    polynomial_array = np.array([ __bernstein_poly(i, nPoints-1, t) for i in range(0, nPoints)   ])
+
+    xvals = np.dot(xPoints, polynomial_array)
+    yvals = np.dot(yPoints, polynomial_array)
+
+    return xvals, yvals
